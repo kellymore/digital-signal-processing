9334367a05603b8d0d01c211bb32017647cd1acc
Kelly Moreira <kelly@Kellys-Air.lan>
2024-05-02 12:14:55 -0400

    clearer vizualization

--- analysis.py
+++ analysis.py
@@ -3,7 +3,7 @@ import librosa.display
 import matplotlib.pyplot as plt
 
 # Load the audio file
-audio_path = 'audio-files/alex-fox-guitar-on-fire.mp3'
+audio_path = 'audio_files/fredericchopin_ nocturne.mp3'
 y, sr = librosa.load(audio_path)
 
 # Compute the Short-time Fourier Transform (STFT)
@@ -13,7 +13,7 @@ D = librosa.stft(y)
 D_db = librosa.amplitude_to_db(abs(D))
 
 # Plot the spectrogram
-librosa.display.specshow(D_db, sr=sr, x_axis='time', y_axis='linear')
+librosa.display.specshow(D_db, sr=sr, x_axis='time', y_axis='log',  cmap='plasma')
 plt.colorbar(format='%+2.0f dB')
 plt.title('Spectrogram')
 plt.show()
